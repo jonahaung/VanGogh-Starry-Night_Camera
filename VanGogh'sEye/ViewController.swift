9d03aad32e0a9c8db493a1378ca5a52185d06fdd
Aung Ko Min <jonahaung@gmail.com>
2020-01-02 00:45:35 +0800

    Initial Commit

---
+++ VanGogh'sEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VanGogh'sEye
+//
+//  Created by Aung Ko Min on 2/1/20.
+//  Copyright © 2020 Aung Ko Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
